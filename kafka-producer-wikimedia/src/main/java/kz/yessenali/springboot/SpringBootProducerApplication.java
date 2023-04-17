3594a46f9dad23c7d2e25c78befc9754ebc20b5b
yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-17 22:40:34 +0600

    first commit

---
+++ kafka-producer-wikimedia/src/main/java/kz/yessenali/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,12 @@
+package kz.yessenali.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
---
+++ src/test/java/kz/yessenali/springbootkafkarealworldproject/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yessenali.springbootkafkarealworldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
