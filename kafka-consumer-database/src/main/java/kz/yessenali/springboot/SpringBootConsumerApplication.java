f539aff7b28c494f6df0ff1472cbca071f35a916
yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-18 10:28:37 +0600

    created kafka consumer project

---
+++ kafka-consumer-database/src/main/java/kz/yessenali/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,12 @@
+package kz.yessenali.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
--- kafka-producer-wikimedia/src/main/java/kz/yessenali/springboot/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/kz/yessenali/springboot/SpringBootProducerApplication.java
@@ -1,12 +1,22 @@
 package kz.yessenali.springboot;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
